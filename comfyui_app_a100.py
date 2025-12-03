ca9cd7e9ab8c712a9787142257568b6a9511ffe3
hiddenflow <69741367+hiddenflow@users.noreply.github.com>
2025-12-03 10:24:01 +0700

    update SageAttention

--- comfyui_app_a100.py
+++ comfyui_app_a100.py
@@ -84,7 +84,7 @@ image = image.run_commands([
     "pip install http://github.com/Dao-AILab/flash-attention/releases/download/v2.8.3/flash_attn-2.8.3+cu12torch2.8cxx11abiTRUE-cp312-cp312-linux_x86_64.whl --no-build-isolation",
 #    "pip install triton"
     "git clone https://github.com/thu-ml/SageAttention.git",
-    "cd SageAttention/sageattention",
+    "cd SageAttention/sageattention3_blackwell",
 #    "python3 setup.py install"
     "pip install -e ."
 ])
