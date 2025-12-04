60d8eedcaec027c6e77336357a38f7e248c17493
hiddenflow <69741367+hiddenflow@users.noreply.github.com>
2025-12-04 12:27:37 +0700

    triton

--- comfyui_app_a100.py
+++ comfyui_app_a100.py
@@ -50,7 +50,7 @@ image = (
         "pip install librosa",
         # Install ComfyUI to default location
         "comfy --skip-prompt install --nvidia",
-        "pip install torch==2.8.0+cu129 torchvision==0.23.0+cu129 torchaudio==2.8.0+cu129 xformers==0.0.30 --index-url https://download.pytorch.org/whl/cu129 --force-reinstall",
+        "pip install torch==2.8.0+cu129 torchvision==0.23.0+cu129 torchaudio==2.8.0+cu129 xformers==0.0.30 triton==3.4.0 --index-url https://download.pytorch.org/whl/cu129 --force-reinstall",
     ])
     .env({"HF_HUB_ENABLE_HF_TRANSFER": "1"})
 )
