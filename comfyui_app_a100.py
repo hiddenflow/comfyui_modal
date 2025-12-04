7573dfe7c3c169251297659a5ce4bc0c92062b8d
hiddenflow <69741367+hiddenflow@users.noreply.github.com>
2025-12-04 10:52:47 +0700

    change torch

--- comfyui_app_a100.py
+++ comfyui_app_a100.py
@@ -83,7 +83,7 @@ image = image.run_commands([
     "pip install psutil",
     "pip install packaging",
     "pip install wheel",
-    "pip install torch==2.8.0 torchvision==0.23.0 torchaudio==2.8.0 --index-url https://download.pytorch.org/whl/cu129 --force-reinstall",
+    "pip install torch==2.8.0+cu129 torchvision==0.23.0+cu129 torchaudio==2.8.0+cu129 --index-url https://download.pytorch.org/whl/cu129 --force-reinstall",
     "pip install -r http://raw.githubusercontent.com/hiddenflow/crossOS_acceleritor/refs/heads/main/acceleritor_torch280cu129_lite.txt",
     "pip list"
 #    "pip --upgrade --force-reinstall torch==2.10.0.dev20251016+cu129 torchvision==0.25.0.dev20251016+cu129 torchaudio==2.8.0.dev20251016+cu129 --index-url https://download.pytorch.org/whl/nightly/cu129",
