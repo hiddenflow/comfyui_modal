7d13c9eaeb9f223c7d5960f8692b9ee96ff5bdf2
hiddenflow <69741367+hiddenflow@users.noreply.github.com>
2025-12-11 22:56:46 +0700

    SageAttention

--- comfyui_app_a100.py
+++ comfyui_app_a100.py
@@ -133,8 +133,8 @@ image = image.run_commands([
     "pip install psutil",
     "pip install packaging",
     "pip install soxr==0.5.0.post1 --force-reinstall",
-    "pip install sageattention==2.2.0 --no-build-isolation",
-#    "git clone https://github.com/thu-ml/SageAttention.git && cd SageAttention && python setup.py install",
+#    "pip install sageattention==2.2.0 --no-build-isolation",
+    "git clone https://github.com/thu-ml/SageAttention.git && cd SageAttention && pip install -e .",
     "pip install https://github.com/Dao-AILab/flash-attention/releases/download/v2.8.1/flash_attn-2.8.1+cu12torch2.8cxx11abiTRUE-cp312-cp312-linux_x86_64.whl --no-build-isolation"
 #    "git clone https://github.com/Dao-AILab/flash-attention.git && cd flash-attention/hopper && python setup.py install"
 #    "pip install sageattention==2.2.0 --no-build-isolation"
