312690510f18ec6b4d088c69fe1628e9f8816543
hiddenflow <69741367+hiddenflow@users.noreply.github.com>
2025-12-26 11:52:51 +0700

    natten

--- comfyui_app_face_restoration.py
+++ comfyui_app_face_restoration.py
@@ -59,7 +59,8 @@ image = (
         "torch==2.8.0+cu128",
         "torchvision==0.23.0+cu128",
         "torchaudio==2.8.0+cu128",
-        "xformers==0.0.32.post2 triton==3.4.0",
+        "xformers==0.0.32.post2",
+        "triton==3.4.0",
         index_url="https://download.pytorch.org/whl/cu128",
         extra_options="--force-reinstall"
     )
