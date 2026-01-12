e0de7fb3ca01bfcb7863a9354eec2445307ab802
hiddenflow <69741367+hiddenflow@users.noreply.github.com>
2026-01-12 20:32:36 +0700

    av_model

--- comfyui_app_video_ltx2.py
+++ comfyui_app_video_ltx2.py
@@ -289,9 +289,10 @@ def ui():
 
     url_av = "https://raw.githubusercontent.com/kijai/ComfyUI/refs/heads/ltx2_memory/comfy/ldm/lightricks/av_model.py"
     output_av = "comfy/ldm/lightricks/av_model.py"
+    av_command = f"wget -q {url_av} -O {output_av}"
 
     try:
-        subprocess.run(["wget", "-q", url_av, "-O", output_av], shell=True, check=True, capture_output=True, text=True)
+        subprocess.run(av_command, shell=True, check=True, capture_output=True, text=True)
         print(f"Berhasil mengganti file: {output_path}")
     except subprocess.CalledProcessError as e:
         print(f"Gagal mendownload file: {e}")
