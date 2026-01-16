ec7160b114cf837269125d45db06f0a861eb463e
hiddenflow <69741367+hiddenflow@users.noreply.github.com>
2026-01-16 18:35:37 +0700

    gguf

--- comfyui_app_video_ltx2.py
+++ comfyui_app_video_ltx2.py
@@ -23,7 +23,7 @@ def git_clone_cmd(node_repo: str, recursive: bool = False, install_reqs: bool = 
         cmd += " --recursive"
     if install_reqs:
         # cmd += f" && pip install -r {dest}/requirements.txt"
-        cmd += f"grep -vE '^(torch|torchvision|torchaudio|xformers|triton)' {dest}/requirements.txt | pip install -r /dev/stdin"
+        cmd += f"&& grep -vE '^(torch|torchvision|torchaudio|xformers|triton)' {dest}/requirements.txt | pip install -r /dev/stdin"
     if "ComfyUI-Frame-Interpolation" in node_repo:
         cmd += "install.py"
     return cmd
